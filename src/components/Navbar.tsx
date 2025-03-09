e1b2f7450d97e106889679e4e3a3bea96f78c3e0
Shokh-002 <reactjs2002@gmail.com>
2025-03-09 15:50:13 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -149,7 +149,7 @@ export default function Navbar() {
             Murojat qoldirish
           </Button>
       </DialogTrigger>
-      <DialogContent className="w-[320px] top-85 -mx-25   ">
+      <DialogContent className="w-[70%] top-85 -mx-[12%]   ">
       <form action="" className="w-full">
 <DialogHeader>
           <DialogTitle>Texnik qo`llab quvvatlash markazi</DialogTitle>
